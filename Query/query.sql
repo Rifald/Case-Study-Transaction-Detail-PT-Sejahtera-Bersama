6c3fb957f9ddd0275c63cf7daab4755803c03aa8
rifaldiqbal <43335005+Rifald@users.noreply.github.com>
2022-07-02 16:54:05 +0700

    Update query.sql

--- Query/query.sql
+++ Query/query.sql
@@ -8,5 +8,6 @@ SUM(IIF(CategoryName='Robots', orders.Quantity, 0)) AS `Robots`,
 SUM(IIF(CategoryName='Training Videos', orders.Quantity, 0)) AS `Training Videos`,
 SUM(orders.Quantity) AS `Total Order`,
 SUM(products.Price) AS `Total Price Order`
-FROM (productCategory INNER JOIN products ON productCategory.CategoryID = products.Category) INNER JOIN (customers INNER JOIN orders ON customers.CustomerID = orders.CustomerID) ON products.ProdNumber = orders.ProdNumber
+FROM (productCategory INNER JOIN products ON productCategory.CategoryID = products.Category) 
+INNER JOIN (customers INNER JOIN orders ON customers.CustomerID = orders.CustomerID) ON products.ProdNumber = orders.ProdNumber
 GROUP BY YEAR(orders.Date);
